1ef4bff577db22a7cc6e0966f6df6fcbd37a8084
damiron4 <damiron3222@gmail.com>
2022-09-17 14:17:34 +0600

    Added comments in main

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var http = require('http'); // setting module(library) named 'http'
-var dt = require('./time_module');
+var dt = require('./time_module'); // added custom module from time_module.js
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.write(req.url);
+    res.write("The date and time are currently: " + dt.myDateTime()); //displaying current time
+    res.write(req.url); 
     res.end();
-  }).listen(8080);
+  }).listen(8080); // port name
