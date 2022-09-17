85ace8b6478cd79cb2c6e314f4765ba1db890d61
damiron4 <damiron3222@gmail.com>
2022-09-17 13:14:15 +0600

    local server test

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 //hmmmmm
 //asdadsa
 //asda
+var http = require('http');
+
+http.createServer(funciton(req, res) {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8000);
