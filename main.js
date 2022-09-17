028ca36d3286a04d868195c67f92c36f0c47de88
Zhaksylyk <zhk27aa@gmail.com>
2022-09-17 13:55:15 +0600

    111

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var http = require('http'); // setting module(library) named 'http'
 var dt = require('./time_module');
-//ali 4ert
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time are currently: " + dt.myDateTime());
