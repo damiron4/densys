47056c9a183355a6213b6f03532b6552f87c56f3
Zhaksylyk <zhk27aa@gmail.com>
2022-11-03 15:44:19 +0600

    datepciker

--- react_front/src/form.js
+++ react_front/src/form.js
@@ -4,6 +4,8 @@ export default function Form() {
 
 // States for registration
 const [name, setName] = useState('');
+const [surname, setSurName] = useState('');
+const [midname, setMidName] = useState('');
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 
@@ -16,7 +18,15 @@ const handleName = (e) => {
 	setName(e.target.value);
 	setSubmitted(false);
 };
+const handleSurName = (e) => {
+	setSurName(e.target.value);
+	setSubmitted(false);
+};
 
+const handleMidName = (e) => {
+	setMidName(e.target.value);
+	setSubmitted(false);
+};
 // Handling the email change
 const handleEmail = (e) => {
 	setEmail(e.target.value);
@@ -32,7 +42,7 @@ const handlePassword = (e) => {
 // Handling the form submission
 const handleSubmit = (e) => {
 	e.preventDefault();
-	if (name === '' || email === '' || password === '') {
+	if (name === '' || email === '' || password === ''|| surname === '' || midname === '') {
 	setError(true);
 	} else {
 	setSubmitted(true);
@@ -83,6 +93,14 @@ return (
 		<label className="label">Name</label>
 		<input onChange={handleName} className="input"
 		value={name} type="text" />
+
+		<label className="label">Surname</label>
+		<input onChange={handleSurName} className="input"
+		value={surname} type="text" />
+
+		<label className="label">Middlename</label>
+		<input onChange={handleMidName} className="input"
+		value={midname} type="text" />
 
 		<label className="label">Email</label>
 		<input onChange={handleEmail} className="input"
