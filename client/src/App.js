6b4e0050484334ec99f12388446d95a77a68a3bd
KotPL <ali.kanafin@nu.edu.kz>
2022-11-06 23:41:52 +0600

    fjfjh

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,12 @@
-import React, {Component} from "react";
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
-import Form from "./Form"
-import LoginPage from "./LoginPage"
-import RegistrationDoctor from "./RegistrationDoctor"
-
-
-
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-
-    };
-  }
-
-  render(){
-    return(
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">LoginPage</Link>
-              </li>
-              <li>
-                <Link to="/Form">Form</Link>
-              </li>
-              <li>
-                <Link to="/RegistrationDoctor">RegistrationDoctor</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path="/" Component={LoginPage}/>
-            <Route exact path="/Form" Component={Form}/>
-            <Route exact path="/RegistrationDoctor" Component={RegistrationDoctor}/>
-          </Switch>
-        </div>
-      </Router>
-    )
-  }
-}
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Form />
-//     </div>
+import Com from "./Com.js"
+function App() {
+  return (
+    <div className="App">
+      <Com />
+    </div>
      
-//   );
-// }
+  );
+}
  
 export default App;
--- client/src/loginPage.js
+++ client/src/loginPage.js
@@ -4,20 +4,120 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export default function Form() {
 
-const [iin, setiin] = useState('');
-const [password, setPassword] = useState('');
-const [submitted, setSubmitted] = useState(false);
-const [error, setError] = useState(false);
-
-const handleiin = (e) => {
-	setiin(e.target.value);
-	setSubmitted(false);
-}
-
-const handlePassword = (e) => {
-	setPassword(e.target.value);
-	setSubmitted(false);
-}
+	const [name, setName] = useState('');
+	const [surname, setSurName] = useState('');
+	const [midname, setMidName] = useState('');
+	const [iin, setiin] = useState('');
+	const [id, setid] = useState('');
+	const [email, setEmail] = useState('');
+	const [adress, setAdress] = useState('');
+	const [contactn, setContactn] = useState('');
+	const [dbirth, setDbirth] = useState(new Date());
+	const current = new Date();
+	const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+	const [password, setPassword] = useState('');
+	const[depid, SetDepid] =  useState('');
+	const[specid, SetSpecid] =  useState('');
+	const[exper, SetExper] =  useState('');
+	const[price, SetPrice] =  useState('');
+	const[category, SetCategory] =  useState('');
+	const[degree, SetDegree] =  useState('');
+	const[rating, SetRating] =  useState('');
+	const[photo, SetPhoto] =  useState('');
+	
+	
+	const [submitted, setSubmitted] = useState(false);
+	const [error, setError] = useState(false);
+	
+	const handleName = (e) => {
+		setName(e.target.value);
+		setSubmitted(false);
+	};
+	const handleSurName = (e) => {
+		setSurName(e.target.value);
+		setSubmitted(false);
+	};
+	
+	const handleMidName = (e) => {
+		setMidName(e.target.value);
+		setSubmitted(false);
+	};
+	
+	const handleiin = (e) => {
+		setiin(e.target.value);
+		setSubmitted(false);
+	}
+	
+	const handleid = (e) => {
+		setid(e.target.value);
+		setSubmitted(false);
+	}
+	
+	const handleDbirth = (e) => {
+		setDbirth(e.target.value.toUpperCase());
+		setSubmitted(false);
+	}
+	
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+		setSubmitted(false);
+	}
+	
+	const handlePassword = (e) => {
+		setPassword(e.target.value);
+		setSubmitted(false);
+	}
+	const handleAdress = (e) => {
+		setAdress(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handleContactn = (e) => {
+		setContactn(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handleDepid = (e) => {
+		SetDepid(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handleSpecid = (e) => {
+		SetSpecid(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handleExper = (e) => {
+		SetExper(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handlePrice = (e) => {
+		SetPrice(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	
+	const handleCategory = (e) => {
+		SetCategory(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handleDegree = (e) => {
+		SetDegree(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handleRating = (e) => {
+		SetRating(e.target.value);
+		setSubmitted(false);	
+	}
+	
+	const handlePhoto = (e) => {
+		console.log(e.target.files)
+		SetPhoto(e.target.files[0]);
+		setSubmitted(false);	
+	}
 const handleSubmit = (e) => {
 	e.preventDefault();
 	if (iin === '' || password === '' ) {
