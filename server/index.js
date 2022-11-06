bf655af9bb1926807b0800fae7b7c6f704d7ebcb
damiron4 <damiron3222@gmail.com>
2022-11-06 21:32:38 +0600

    added doctor registration

--- server/index.js
+++ server/index.js
@@ -11,59 +11,66 @@ app.use(express.json());
 // TODO: Write Get/Post/Update routes
 
 app.post("/register-patient", async (req, res) => {
-    try {
-        check = true
-        console.log(req.body);
-        const {date, IIN, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date} = req.body;
-        await pool.query("INSERT INTO patient VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
-        [date, IIN, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date],
-        (err, result) => {
+    check = true
+    console.log(req.body);
+    const {date, IIN, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date} = req.body;
+    await pool.query("INSERT INTO patient VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+    [date, IIN, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date],
+    (err, result) => {
+        if (err) {
+            res.json({ err: err});
+        }
+        // console.log(result);
+        if (result.rowCount == 0) {
+            check = false
+            res.json({ message: "Incorrect values."});
+        }
+        
+    })
+    if (check) {
+        const password = Math.random().toString(36).slice(2, 10);
+        await pool.query("INSERT INTO auth_patient VALUES($1, $2)", [IIN, password], (err, result) => {
             if (err) {
                 res.json({ err: err});
             }
             // console.log(result);
-            if (result.rowCount == 0) {
-                check = false
-                res.json({ message: "Incorrect values."});
+            if (result.rowCount > 0) {
+                res.json({ message: "Registration successful", login: IIN, password: password});
             }
-            
-        })
-        if (check) {
-            const password = Math.random().toString(36).slice(2, 10);
-            await pool.query("INSERT INTO auth_patient VALUES($1, $2)", [IIN, password], (err, result) => {
-                if (err) {
-                    res.json({ err: err});
-                }
-                // console.log(result);
-                if (result.rowCount > 0) {
-                    res.json({ message: "Registration successful", login: IIN, password: password});
-                }
-            });
-        } 
-    } catch (error) {
-        console.log(error.message);
-    }
+        });
+    } 
 });
 
 app.post("/register-doctor", async (req, res) => {
-    try {
-        console.log(req.body);
-        const {date, IIN, id, name, surname, middlename, contact, dep_id, special_id, exp, photo, ctg, price, schedule, degree, rating, address, homepage_url} = req.body;
-        await pool.query("INSERT INTO doctor VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)", [date, IIN, id, name, surname, middlename, contact, dep_id, special_id, exp, photo, ctg, price, schedule, degree, rating, address, homepage_url], (err, result) => {
+    check = true
+    console.log(req.body);
+    const {date, IIN, id, name, surname, middlename, contact, dep_id, special_id, exp, photo, ctg, price, schedule, degree, rating, address, homepage_url} = req.body;
+    await pool.query("INSERT INTO doctor VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)", 
+    [date, IIN, id, name, surname, middlename, contact, dep_id, special_id, exp, photo, ctg, price, schedule, degree, rating, address, homepage_url],
+    (err, result) => {
+        if (err) {
+            res.json({ err: err});
+        }
+        // console.log(result);
+        if (result.rowCount == 0) {
+            check = false
+            res.json({ message: "Incorrect values."});
+        }
+    });
+    if (check) {
+        const password = Math.random().toString(36).slice(2, 10);
+        await pool.query("INSERT INTO auth_doctor VALUES($1, $2)", [IIN, password], (err, result) => {
             if (err) {
                 res.json({ err: err});
             }
+            // console.log(result);
             if (result.rowCount > 0) {
-                res.json({ message: "Registration successful."});
-            } else {
-                res.json({ message: "Incorrect values."});
+                res.json({ message: "Registration successful", login: IIN, password: password});
             }
-        })
-        // const newUser = await pool.query("INSERT INTO auth VALUES($1,$2) RETURNING *", [iin, pass])        
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+        });
+    } 
+    // const newUser = await pool.query("INSERT INTO auth VALUES($1,$2) RETURNING *", [iin, pass])        
+});
 
 
 
@@ -127,25 +134,7 @@ app.post("/login-patient", async (req, res) => {
     }
 })
 
-app.post("/register-patient", async (req, res) => {
-    try {
-    console.log(req.body);
-    const {login, pass} = req.body;
-    
-    } catch (error) {
-        console.log(error.message)
-    }
-})
-
-app.post("/register-doctor", async (req, res) => {
-    try {
-    console.log(req.body);
-    const {login, pass} = req.body;
-    
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+app.e
 
 app.listen(5000, () => {
     console.log("started on port 5000")
