99bf268aa9f76610aa828aac0388f5c87b2e423a
Zhaksylyk <zhk27aa@gmail.com>
2022-11-06 22:34:45 +0600

    doc and pat

--- client/src/form.js
+++ client/src/form.js
@@ -11,15 +11,22 @@ const [midname, setMidName] = useState('');
 const [iin, setiin] = useState('');
 const [id, setid] = useState('');
 const [email, setEmail] = useState('');
-const [bloodg, setBloodg] = useState('');
-const [mstatus, setMstatus] = useState('');
 const [adress, setAdress] = useState('');
-const [emerg, setEmerg] = useState('');
 const [contactn, setContactn] = useState('');
 const [dbirth, setDbirth] = useState(new Date());
 const current = new Date();
 const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 const [password, setPassword] = useState('');
+const[depid, SetDepid] =  useState('');
+const[specid, SetSpecid] =  useState('');
+const[exper, SetExper] =  useState('');
+const[price, SetPrice] =  useState('');
+const[category, SetCategory] =  useState('');
+const[degree, SetDegree] =  useState('');
+const[rating, SetRating] =  useState('');
+const[photo, SetPhoto] =  useState('');
+
+
 const [submitted, setSubmitted] = useState(false);
 const [error, setError] = useState(false);
 
@@ -47,11 +54,6 @@ const handleid = (e) => {
 	setSubmitted(false);
 }
 
-const handlebloodg = (e) => {
-	setBloodg(e.target.value);
-	setSubmitted(false);
-}
-
 const handleDbirth = (e) => {
 	setDbirth(e.target.value.toUpperCase());
 	setSubmitted(false);
@@ -71,24 +73,56 @@ const handleAdress = (e) => {
 	setSubmitted(false);	
 }
 
-const handleEmerg = (e) => {
-	setEmerg(e.target.value);
-	setSubmitted(false);	
-}
-
 const handleContactn = (e) => {
 	setContactn(e.target.value);
 	setSubmitted(false);	
 }
 
-const handleMstatus = (e) => {
-	setMstatus(e.target.value);
+const handleDepid = (e) => {
+	SetDepid(e.target.value);
+	setSubmitted(false);	
+}
+
+const handleSpecid = (e) => {
+	SetSpecid(e.target.value);
+	setSubmitted(false);	
+}
+
+const handleExper = (e) => {
+	SetExper(e.target.value);
+	setSubmitted(false);	
+}
+
+const handlePrice = (e) => {
+	SetPrice(e.target.value);
+	setSubmitted(false);	
+}
+
+
+const handleCategory = (e) => {
+	SetCategory(e.target.value);
+	setSubmitted(false);	
+}
+
+const handleDegree = (e) => {
+	SetDegree(e.target.value);
+	setSubmitted(false);	
+}
+
+const handleRating = (e) => {
+	SetRating(e.target.value);
+	setSubmitted(false);	
+}
+
+const handlePhoto = (e) => {
+	console.log(e.target.files)
+	SetPhoto(e.target.files[0]);
 	setSubmitted(false);	
 }
 
 const handleSubmit = (e) => {
 	e.preventDefault();
-	if (name === '' || password === ''|| surname === '' || midname === '' || iin === '' || bloodg === '' || emerg === '' || id === '' || bloodg === '...' || mstatus === '...' || adress === '' ) {
+	if (name === '' || password === ''|| surname === '' || midname === '' || iin === '' || id === '' || adress === '' || degree === '...' || photo === '') {
 	setError(true);
 	} else {
 	setSubmitted(true);
@@ -136,9 +170,8 @@ return (
 	
 	<section class= "features">
 		<div>
-			<h1>Pacient Registration</h1>
+			<h1>Doctor Registration</h1>
 		</div>
-		{}
 		<label className="label">Name</label>
 		<input onChange={handleName} className="input"
 		value={name} type="text" />
@@ -158,7 +191,7 @@ return (
 			
 
 		<label className="label">IIN number</label>
-		<input maxLength={12}
+		<input maxLength={12} minLength={12}
 		onChange={handleiin} className="input" 
 		value={iin} type="number" />
 
@@ -166,38 +199,44 @@ return (
 		<input onChange={handleid} className="input" 
 		value={id} type="number" />
 
-		<label className="label">Blood group</label>	
-		<select
-			onChange={handlebloodg} 
-			value={bloodg}
-		>	
-			<option>...</option>
-			<option>A+</option>
-			<option>A-</option>
-			<option>B+</option>
-			<option>B-</option>
-			<option>AB+</option>
-			<option>AB-</option>
-			<option>O+</option>
-			<option>O-</option>
-		</select> 
-
 		<label className="label">Contact number</label>
 		<input onChange={handleContactn} className="input"
 		value={contactn} type="number" />
 
-		<label className="label">Emergency contact number</label>
-		<input onChange={handleEmerg} className="input"
-		value={emerg} type="number" />
+		<label className="label">Department ID</label>
+		<input onChange={handleDepid} className="input"
+		value={depid} type="number" />
 
-		<label className="label">Maritial status</label>	
+		<label className="label">Specialization details ID</label>
+		<input onChange={handleSpecid} className="input"
+		value={specid} type="number" />
+
+		<label className="label">Experience in years</label>
+		<input onChange={handleExper} className="input"
+		value={exper} type="number" />
+
+		<label className="label">Upload a photo</label>
+		<input type="file" name='file'
+		onChange={handlePhoto}/>
+		
+
+
+		<label className="label">Category</label>
+		<input onChange={handleCategory} className="input"
+		value={category} type="text" />
+
+		<label className="label">Price of the appointment (Tenge)</label>
+		<input onChange={handlePrice} className="input"
+		value={price} type="number" />
+
+		<label className="label">Degree/education</label>	
 		<select
-			onChange={handleMstatus} 
-			value={mstatus}
+			onChange={handleDegree} 
+			value={degree}
 		>	
 			<option>...</option>
-			<option>Single</option>
-			<option>Married</option>
+			<option>Master's Degree</option>
+			<option>PhD</option>
 		</select>
 
 		<label className="label">Email</label>
@@ -207,6 +246,11 @@ return (
 		<label className="label">Adress</label>
 		<input onChange={handleAdress} className="input"
 		value={adress} type="text" />
+
+		<label className="label">Rating (a number in range from 0 to 10)</label>
+		<input onChange={handleRating} className="input"
+		min={1} max={10}
+		value={rating} type="number" />
 
 		<label className="label">Registration date: {date} </label>
 		
