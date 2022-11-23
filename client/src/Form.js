a234d8f649e362779a05efa73da12c3ad5007399
KotPL <ali.kanafin@nu.edu.kz>
2022-11-23 18:47:22 +0600

    bn

---
+++ client/src/Form.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function Form() {
+
+	
+return (
+	
+	<div>
+	<header className="site-header">
+		<div class="container">
+			<p><ht class="back-ht">A-Clinic</ht></p>
+      		<p>Main Page</p>
+      		<p>Message</p>
+      		<p>Health Care Services</p>
+		</div>
+	</header>
+
+	
+	<section class= "features">
+		
+		
+	</section>
+	<footer class="site-footer">
+      <div class="con">
+        <p>© A-Clinic</p>
+        <p>Welcome to A-Clinic, Health Care website</p>
+      </div>
+    </footer>
+	</div>
+);
+}
