d1a4672133a1abbe38b4d19363cccf98acc25cec
damiron4 <damiron3222@gmail.com>
2022-11-26 19:14:57 +0600

    Fields change

--- server/index.js
+++ server/index.js
@@ -13,58 +13,66 @@ app.use(express.json());
 
 app.post("/register/patient", async (req, res) => {
     try {
-        check = true
-        console.log(req.body);
-        const { date, IIN, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date } = req.body;
-        await pool.query("INSERT INTO patient VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
-            [date, IIN, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date],
-            (err, result) => {
-                if (err) {
-                    res.json({ err: err });
+        console.log("Patient info received");
+        const { dbirth, iin, id, name, surname, midname, bloodg, emerg, contactn, email, address, mstatus, dreg} = req.body;
+        pool.query("INSERT INTO patient VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+            [dbirth, iin, id, name, surname, midname, bloodg, emerg, contactn, email, address, mstatus, dreg],
+            async (error) => {
+                if (!error) {
+                    const password = Math.random().toString(36).slice(2, 10);
+                    pool.query("INSERT INTO auth_patient VALUES($1, $2)", [iin, password], (err, result) => {
+                        if (err) {
+                            res.json({ err: err });
+                        }
+                        if (result.rowCount > 0) {
+                            res.send({
+                                message: "Patient registration is successful.",
+                                username: iin,
+                                password: password
+                            });
+                        }
+                    });
                 }
-                // console.log(result);
-                if (result.rowCount == 0) {
-                    check = false
-                    res.json({ message: "Incorrect values." });
+                else {
+                    res.send({ message: error.message });
                 }
-
-            })
-        if (check) {
-            const password = Math.random().toString(36).slice(2, 10);
-            await pool.query("INSERT INTO auth_patient VALUES($1, $2)", [IIN, password], (err, result) => {
-                if (err) {
-                    res.json({ err: err });
-                }
-                // console.log(result);
-                if (result.rowCount > 0) {
-                    res.json({ message: "Registration successful", login: IIN, password: password });
-                }
-            });
-        }
-    } catch (err) {
-        console.error(err.message);
+            }
+        );
+    } catch (error) {
+        console.error(error.message);
     }
 });
 
 app.post("/register/doctor", async (req, res) => {
     try {
-        console.log("Doctor info recieved")
-        const {dbirth, iin, id, name, surname, midname, contactn, depid, specid, exper, photo, category, price, scheduledetails, degree, rating, address, hpurl} = req.body;
-        await pool.query("INSERT INTO doctor VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)",
+        console.log("Doctor info received");
+        const { dbirth, iin, id, name, surname, midname, contactn, depid, specid, exper, photo, category, price, scheduledetails, degree, rating, address, hpurl } = req.body;
+        pool.query("INSERT INTO doctor VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)",
             [dbirth, iin, id, name, surname, midname, contactn, depid, specid, exper, photo, category, price, scheduledetails, degree, rating, address, hpurl],
-            (error) => {
+            async (error) => {
                 if (!error) {
-                    res.send({status: 1 , message: "Doctor registration successful."});
+                    const password = Math.random().toString(36).slice(2, 10);
+                    pool.query("INSERT INTO auth_patient VALUES($1, $2)", [iin, password], (err, result) => {
+                        if (err) {
+                            res.json({ err: err });
+                        }
+                        if (result.rowCount > 0) {
+                            res.send({
+                                message: "Doctor registration is successful.",
+                                username: iin,
+                                password: password
+                            });
+                        }
+                    });
                 } else {
-                    msg = error.message;
-                    res.send({status: 0, message: msg});
+                    res.send({ message: error.message });
                 }
             }
         );
     } catch (error) {
         console.error(error.message)
     }
-    
+
 });
 
 
@@ -76,14 +84,14 @@ app.post("/login/admin", async (req, res) => {
         const { username, password } = req.body;
         const result = await pool.query("SELECT * FROM auth WHERE login = $1 and pass = $2", [username, password])
         if (result.rowCount != 0) {
-            res.send({message: "Login successful."});
+            res.send({ message: "Login successful." });
         } else {
-            res.send({message: "Wrong username or password."});
+            res.send({ message: "Wrong username or password." });
         }
     } catch (error) {
         console.log(error.message);
     }
-    
+
     // console.log(req.body);
     // const result = await pool.query("SELECT * FROM auth WHERE login = $1 and pass = $2",
     // [username, password]),
@@ -95,13 +103,13 @@ app.post("/login/admin", async (req, res) => {
     //     }
     //     console.log(result);
     //     if (result.rowCount > 0) {
-            
+
     //         res.send({message: "Login successful"})
     //         // res.send({status: 1 , message: "Login successful."});
     //     } else {
     //         res.send({message: "Wrong username or password"});
     //     }}
-    }
+}
 )
 
 app.post("/login/doctor", async (req, res) => {
@@ -164,8 +172,8 @@ app.get("/doctor", async (req, res) => {
 
 app.get("/patient/:id", async (req, res) => {
     try {
-        const IIN = req.params.id;
-        const allInfo = await pool.query("SELECT * FROM patient WHERE IIN = $1", [IIN]);
+        const iin = req.params.id;
+        const allInfo = await pool.query("SELECT * FROM patient WHERE iin = $1", [iin]);
         res.json(allInfo.rows);
     } catch (err) {
         console.error(err.message);
@@ -174,8 +182,8 @@ app.get("/patient/:id", async (req, res) => {
 
 app.get("/doctor/:id", async (req, res) => {
     try {
-        const IIN = req.params.id;
-        const allInfo = await pool.query("SELECT * FROM doctor WHERE IIN = $1", [IIN]);
+        const iin = req.params.id;
+        const allInfo = await pool.query("SELECT * FROM doctor WHERE iin = $1", [iin]);
         res.json(allInfo.rows);
     } catch (err) {
         console.error(err.message);
@@ -184,12 +192,12 @@ app.get("/doctor/:id", async (req, res) => {
 
 app.put("/patient/:id", async (req, res) => {
     try {
-        const IIN = req.params.id;
-        //add IIN
+        const iin = req.params.id;
+        //add iin
         const { date, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date } = req.body;
         const updatePatientInfo = await pool.query(
-            "UPDATE patient SET Bdate = $1, ID_number = $2, Fname = $3, Sname = $4, Mname = $5, Bgroup = $6, Econtact_number = $7, Contact_number = $8, Email = $9, Address = $10, Mstatus = $11, Rdate = $12 WHERE IIN = $13",
-            [date, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date, IIN]
+            "UPDATE patient SET Bdate = $1, ID_number = $2, Fname = $3, Sname = $4, Mname = $5, Bgroup = $6, Econtact_number = $7, Contact_number = $8, Email = $9, Address = $10, Mstatus = $11, Rdate = $12 WHERE iin = $13",
+            [date, id, name, surname, middlename, b_group, emer_contact, contact, email, address, marital_stat, reg_date, iin]
         );
 
         res.json("Patient was updated!");
@@ -200,13 +208,13 @@ app.put("/patient/:id", async (req, res) => {
 
 app.put("/doctor/:id", async (req, res) => {
     try {
-        const IIN = req.params.id;
-        //add IIN
+        const iin = req.params.id;
+        //add iin
         const { date, id, name, surname, middlename, contact, dep_id, special_id, exp, photo, ctg, price, schedule, degree, rating, address, homepage_url }
             = req.body;
         const updatePatientInfo = await pool.query(
-            "UPDATE doctor SET Bdate = $1, ID_number = $2, Fname = $3, Sname = $4, Mname = $5, Contact_number = $6, Department_ID = $7, Specialization_details_ID = $8, Experience = $9, Photo = $10, Category = $11, Price = $12, Schedule_details = $13, Education = $14, Rating = $15, Address = $16, Homepage_URL = $17 WHERE IIN = $18",
-            [date, id, name, surname, middlename, contact, dep_id, special_id, exp, photo, ctg, price, schedule, degree, rating, address, homepage_url, IIN]
+            "UPDATE doctor SET Bdate = $1, ID_number = $2, Fname = $3, Sname = $4, Mname = $5, Contact_number = $6, Department_ID = $7, Specialization_details_ID = $8, Experience = $9, Photo = $10, Category = $11, Price = $12, Schedule_details = $13, Education = $14, Rating = $15, Address = $16, Homepage_URL = $17 WHERE iin = $18",
+            [date, id, name, surname, middlename, contact, dep_id, special_id, exp, photo, ctg, price, schedule, degree, rating, address, homepage_url, iin]
         );
         res.json("Doctor was updated!");
     } catch (err) {
