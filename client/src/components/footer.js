cf6e609e563fb17f97e5a0b87cf27b6e1b358130
Dias <dias.kabykesh@gmail.com>
2022-11-26 23:12:20 +0600

    change

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function accessDenied() {
     return (
     <footer className="site-footer">
-        <div className="container">
+        <div className="con">
             <p>© A-Clinic</p>
             <p>Welcome to A-Clinic, Health Care website</p>
         </div>
