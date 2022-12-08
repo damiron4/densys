cb8bded1806df6af8f8e32fdceeb35e710b9e517
KotPL <ali.kanafin@nu.edu.kz>
2022-12-08 12:09:29 +0600

    footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,19 +1,127 @@
-import React from "react";
-import Row from 'react-bootstrap/Row';
+import React from 'react';
+import { CDBFooter, CDBBox, CDBFooterLink, CDBBtn, CDBIcon, CDBContainer } from 'cdbreact';
+import logo from '../image/densys2.png'
 
 export default function footer() {
-    return (
-     <footer class="bg-light text-center text-lg-start fixed-bottom bg-dark bg-gradient">
-    <div class="text-center p-3 text-light" >
-    <Row>
-      <p class="text-light"> © Densys    Welcome to Densys, Health Care website</p>
-    </Row>
-     </div>
-    </footer>
+  return (
+<footer class="text-center text-lg-start bg-light text-muted">
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
 
-    // <footer className="site-footer">
-    //     <p>© Densys</p>
-    //     <p>Welcome to Densys, Health Care website</p>
-    // </footer>
-    )
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </section>
+
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+             <img src={logo} className="logo rounded-circle position-relative" />
+             <i class=""></i>Densys
+          </h6>
+          <p>
+            Hello
+          </p>
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+ 
+  <div class="text-center p-4">
+    © 2022 Densys:
+    <p class="text-reset fw-bold" >Welcome to Densys, Health Care Clinic</p>
+  </div>
+  
+</footer>
+  );
 };
+
+// import React from "react";
+// import Row from 'react-bootstrap/Row';
+
+// export default function footer() {
+//     return (
+//      <footer class="bg-light text-center text-lg-start fixed-bottom bg-dark bg-gradient">
+//     <div class="text-center p-1 text-light" >
+//     <Row>
+//       <p class="text-light"> © Densys    Welcome to Densys, Health Care website</p>
+//     </Row>
+//      </div>
+//     </footer>
+
+//     // <footer className="site-footer">
+//     //     <p>© Densys</p>
+//     //     <p>Welcome to Densys, Health Care Clinic</p>
+//     // </footer>
+//     )
+// };
